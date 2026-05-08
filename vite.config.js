e75382ef1d7df653890ade49637386d43b453e4b
singhal-ansh <anshsinghal0507@gmail.com>
2026-05-08 17:54:21 +0530

    vite.config.js edit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
-  base: '/Singhal-ansh.github.io/'
+  base: '/'   // ← change this
 })
